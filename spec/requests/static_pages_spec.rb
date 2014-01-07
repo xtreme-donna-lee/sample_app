242381204b6fb8e48aa896bda353eed139ab82be
Donna Lee <donna.lee@xtremelabs.com>
2014-01-07 11:05:31 -0500

    End of 3.3

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,6 +7,11 @@ describe "Static pages" do
 			visit '/static_pages/home'
 			expect(page).to have_content('Sample App')
 		end
+
+		it "should have the title 'Home'" do
+      			visit '/static_pages/home'
+      			expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+    		end
 	end
 
 	describe "Help page" do
@@ -14,6 +19,12 @@ describe "Static pages" do
 			visit '/static_pages/help'
 			expect(page).to have_content('Help')
 		end
+	
+
+		it "should have the title 'Help'" do
+      			visit '/static_pages/help'
+      			expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+    		end
 	end
 
 	describe "About page" do
@@ -21,6 +32,11 @@ describe "Static pages" do
 			visit '/static_pages/about'
 			expect(page).to have_content('About Us')
 		end
+
+		it "should have the title 'About Us'" do
+      			visit '/static_pages/about'
+      			expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+    		end
 	end
 
 end
